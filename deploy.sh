d14c99a21c4936c6d7f80ba40727da82268b29a6
Admin <rick_broker@yahoo.com>
2020-02-06 20:42:15 -0500

    change lines

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
-openssl aes-256-cbc -K $encrypted_e5bd312d25bf_key -iv $encrypted_e5bd312d25bf_iv
-  -in deploy_key.enc -out /tmp/deploy_key -d
+openssl aes-256-cbc -K $encrypted_e5bd312d25bf_key -iv $encrypted_e5bd312d25bf_iv -in deploy_key.enc -out /tmp/deploy_key -d
 eval "$(ssh-agent -s)"
 echo -e "Host $DIST_HOST\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 chmod 600 /tmp/deploy_key
